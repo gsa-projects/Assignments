9d965207011321b80e72d4adbead11123c1309a8
Hyunseung Ryu <56152093+Rhseung@users.noreply.github.com>
2023-05-08 15:23:41 +0900

    Update example.py

--- py_omok/example.py
+++ py_omok/example.py
@@ -12,21 +12,19 @@ board_image = image.load("C:/Users/rhseung/Coding/Repositories/gsa-projects/Assi
 white_stone = image.load("C:/Users/rhseung/Coding/Repositories/gsa-projects/Assignments/py_omok/assets/white_stone.png")    # (30, 30)
 black_stone = image.load("C:/Users/rhseung/Coding/Repositories/gsa-projects/Assignments/py_omok/assets/black_stone.png")    # (30, 30)
 
-stones = [i ]
+# stones = [[None for _ in range(15)] for _ in range(15)]
 
 while True:
     for evt in event.get():
         if evt.type == QUIT:
             quit()
             sys.exit()
-        elif evt.type == MOUSEBUTTONDOWN:
-
-            pass
-
-    white_stone.set_alpha(128)
+        # elif evt.type == MOUSEBUTTONDOWN:
+        #     stones[nth(mouse_y)][nth(mouse_x)] = "white"
 
     mouse_x, mouse_y = mouse.get_pos()
     nth = lambda pos: round((pos - 20) / 40)
+    white_stone.set_alpha(128)    
     screen.blit(white_stone, (20 + 40 * nth(mouse_x) - 15, 20 + 40 * nth(mouse_y) - 15))
     screen.blit(board_image, (0, 0))
 
