c9eceb8c241e6cd61c976c58966a175c991abad9
Hyunseung Ryu <56152093+Rhseung@users.noreply.github.com>
2023-05-23 21:08:36 +0900

    .

--- finance/ex.py
+++ finance/ex.py
@@ -5,13 +5,27 @@ from datetime import datetime
 from matplotlib import dates as mdates
 from bs4 import BeautifulSoup as bs
 
-url = 'https://finance.naver.com/item/sise_day.nhn?code=068270&page=1'
+code = input('종목 코드를 입력하세요: ')
+
 headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'}
-response = requests.get(url, headers=headers)
+df = pd.DataFrame()
+sise_url = f'https://finance.naver.com/item/sise_day.nhn?code={code}'  
+for page in range(1, 100):
+    page_url = '{}&page={}'.format(sise_url, page)
 
-html = bs(response.text, 'html.parser')
-html_table = html.select("table")
-table = pd.read_html(str(html_table))
-print('파싱된 테이블의 개수 :', len(table))
+    print(f'page: {page} / 99')
 
-print(table[0])
+    # 위에서 했던 일련의 과정들을 각 url에 대해서 (99페이지에 대해서 반복)
+    response = requests.get(page_url, headers=headers)
+    html = bs(response.text, 'html.parser')
+    html_table = html.select("table")
+    table = pd.read_html(str(html_table))
+
+    # 현재 얻은 데이터프레임을 기존 데이터프레임에 누적.
+    df = df._append(table[0].dropna())
+
+df = df.dropna()
+df = df.iloc[0:30] 
+df = df.sort_values(by='날짜')
+
+print(df)
