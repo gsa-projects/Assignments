19e596614f514e7c09361db948779dde941a370a
Hyunseung Ryu <56152093+Rhseung@users.noreply.github.com>
2023-05-26 13:35:04 +0900

    .

--- finance/ex.py
+++ finance/ex.py
@@ -1,11 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import requests
+import re
 from datetime import datetime
 from matplotlib import dates as mdates
 from bs4 import BeautifulSoup as bs
 
-code = input('종목 코드를 입력하세요: ')
+print('종목을 불러오고 있습니다...')
+code_df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13')[0]
+code_df.종목코드 = code_df.종목코드.map('{:06d}'.format)
+code_df = code_df[['회사명', '종목코드']]
+stock_list = code_df.values.tolist()
+print('종목을 불러왔습니다.')
+
+tmp = input('종목 코드 혹은 이름을 입력하세요: ')
+try:
+    if re.match(r'^\d{6}$', tmp):
+        code = tmp
+        title = list(filter(lambda x: x[1] == code, stock_list))[0][0]
+    else:
+        code = list(filter(lambda x: x[0] == tmp, stock_list))[0][1]
+        title = tmp
+except IndexError:
+    print('종목을 찾을 수 없습니다.')
+    exit()
+
+print(f'찾은 것: {title}({code})')
 
 headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'}
 df = pd.DataFrame()
@@ -28,11 +48,11 @@ df = df.dropna()
 df = df.iloc[0:30] 
 df = df.sort_values(by='날짜')
 
+plt.rc('font', family='Malgun Gothic')
 plt.figure(figsize=(15, 5))
-plt.title('Celltrion (close)')
+plt.title(title)
 plt.xticks(rotation=45)
 plt.plot(df['날짜'], df['종가'], 'co-')
 plt.grid(color='gray', linestyle='--')
 plt.show()
-
-plt.savefig('savefig_default.png')
+plt.savefig(f'{title}.png')
